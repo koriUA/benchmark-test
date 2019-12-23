40ad13fb203980be85d6233682bd3512975d5681
vitalii <kori.if.ua@gmail.com>
2019-12-23 11:52:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {DataTableSkeleton} from 'carbon-components-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Test...</h1>
       </header>
+      <div className="bx--grid">
+        <div className="bx--row">
+          <div className="bx--col-md-4 bx--col-lg-4">1/3</div>
+          <div className="bx--col-md-4 bx--col-lg-4">1/3</div>
+          <div className="bx--col-md-4 bx--col-lg-4">1/3</div>
+        </div>
+        <div className="bx--row">
+          <div className="bx--col-lg-12">
+            <div
+              style={{
+                width: '100%'
+              }}
+            >
+              <DataTableSkeleton
+                columnCount={5}
+                compact={false}
+                headers={[
+                  'Name',
+                  'Protocol',
+                  'Port',
+                  'Rule',
+                  'Attached Groups'
+                ]}
+                rowCount={5}
+                zebra={false}
+              />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
