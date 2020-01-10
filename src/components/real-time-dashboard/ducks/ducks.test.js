382af857a8ca410d9a0b063508ffbc7149d13a47
vitalii <kori.if.ua@gmail.com>
2020-01-10 11:53:07 +0200

    feat(rtm): initial 001

--- src/components/real-time-dashboard/ducks/ducks.test.js
+++ src/components/real-time-dashboard/ducks/ducks.test.js
@@ -1,6 +1,38 @@
 describe('real-time-dashboard component redux', () => {
   describe('selectors', () => {
-    it('dump test...', () => {
+    it('dump test 001...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 002...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 003...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 004...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 005...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test6...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 007...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 008...', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('dump test 009...', () => {
       expect(true).toEqual(true);
     });
   });
