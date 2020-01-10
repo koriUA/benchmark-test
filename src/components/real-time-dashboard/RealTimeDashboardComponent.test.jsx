5461f150497634c84ccddd00bbe75e4a41475fb8
vitalii <kori.if.ua@gmail.com>
2020-01-10 12:13:45 +0200

    feat(rtm): initial 005

--- src/components/real-time-dashboard/RealTimeDashboardComponent.test.jsx
+++ src/components/real-time-dashboard/RealTimeDashboardComponent.test.jsx
@@ -2,4 +2,16 @@ import RealTimeDashboardComponent from './RealTimeDashboardComponent';
 
 it('text showed correctly', () => {
   console.log(RealTimeDashboardComponent);
+
+  it('dump test 001...', () => {
+    expect(!!RealTimeDashboardComponent).toEqual(true);
+  });
+
+  it('dump test 002...', () => {
+    expect(!!RealTimeDashboardComponent).toEqual(true);
+  });
+
+  it('dump test 003...', () => {
+    expect(true).toEqual(true);
+  });
 });
--- src/components/real-time-dashboard/RealTimeDashboardComponentContainer.test.jsx
+++ src/components/real-time-dashboard/RealTimeDashboardComponentContainer.test.jsx
@@ -2,4 +2,16 @@ import RealTimeDashboardComponentContainer from './RealTimeDashboardComponentCon
 
 it('text showed correctly', () => {
   console.log(RealTimeDashboardComponentContainer);
+
+  it('dump test 001...', () => {
+    expect(!!RealTimeDashboardComponentContainer).toEqual(true);
+  });
+
+  it('dump test 002...', () => {
+    expect(!!RealTimeDashboardComponentContainer).toEqual(true);
+  });
+
+  it('dump test 003...', () => {
+    expect(true).toEqual(true);
+  });
 });
